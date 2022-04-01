10d307e782ed28ee27cacfa8fb24973135a0a031
mudassarwurfel <programmingwithhassan@gmail.comm>
2022-04-01 12:48:45 +0500

    dark and light theme seting

--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class Mytheme{
  
@@ -15,13 +16,21 @@ class Mytheme{
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        brightness: Brightness.light,
+        // brightness: Brightness.light,
         primarySwatch: Colors.blue,
         fontFamily: GoogleFonts.poppins().fontFamily,
+          canvasColor: CreamColor,
+          
+        
+        cardColor: Colors.white,
+        buttonColor: DarkBlue,
+        accentColor: Colors.black,
         appBarTheme: AppBarTheme(
+          
 
           backgroundColor: Colors.white,
           elevation: 0.0,
+        
           iconTheme: IconThemeData(color: Colors.black),
            toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
            titleTextStyle: Theme.of(context).textTheme.headline6,
@@ -35,21 +44,29 @@ class Mytheme{
 
       static ThemeData DarkTheme(BuildContext context)=>ThemeData(
 
-        brightness: Brightness.dark,
+        
         primarySwatch: Colors.blue,
+     
         fontFamily: GoogleFonts.poppins().fontFamily,
+        canvasColor: Mytheme.bluish,
+        accentColor: Colors.white,
+        cardColor: Colors.black,
+        
+            buttonColor: DarkBlue,
         appBarTheme: AppBarTheme(
 
           backgroundColor: Colors.black,
           elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.black),
+          iconTheme: IconThemeData(color: Colors.white),
            toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
            titleTextStyle: Theme.of(context).textTheme.headline6,
       ));
 
       //color
     static Color CreamColor=Color(0xfff5f5f5);
-        static Color DarkBlue=Color(0xff403b58);
+        static Color DarkBlue=Color.fromARGB(255, 43, 1, 255);
+        static Color bluish=Color.fromARGB(255, 13, 6, 44);
               static Color black=Color(00000000);
+               static Color DarkCreamColor=Color.fromARGB(255, 20, 20, 48);
 
 }
